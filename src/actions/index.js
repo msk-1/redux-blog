55e79db35372541957aa903f66a43337ebcf2018
msk1 <48862098+msk-1@users.noreply.github.com>
2020-02-07 00:10:25 +0900

    complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,29 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from 'lodash';
+
+export const fetchPostsAndUseers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+
+  _.chan(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+}
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = function(id) {
+  return _.memoize(async function(dispatch) {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+  });
 };
